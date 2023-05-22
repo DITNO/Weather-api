4648cafe96822a4e9841fac037c3701cd823bd8a
DITNO <92884446+DITNO@users.noreply.github.com>
2023-05-22 20:06:38 +0530

    Update app.js
    added map api

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
+const { log } = require('console');
 const express = require('express');
+const https = require('https');
+
 
 const app = express();
 
 app.get('/',function(req,res){
+
+    const url = 'https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={9932a439138cad58ad570976a21c613e}';
+    https.get(url,function(response){
+        console.log(response);
+    })
     res.send('server is up and running');
-})
+});
 
 app.listen(3000, function(){
     console.log('server is running on the 3000 server');
-})
+});
+
